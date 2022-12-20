8b61bb31468dfce70f47466f91fe81ab66355d13
ibrahimsoltan <70820767+ibrahimsoltan@users.noreply.github.com>
2022-12-20 20:10:56 +0200

    market added with it's realtions note: addproduct edited

--- ecomm-db.sql
+++ ecomm-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 19, 2022 at 03:43 PM
+-- Generation Time: Dec 20, 2022 at 06:09 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `favorite_products` (
   PRIMARY KEY (`favorite_products_id`),
   UNIQUE KEY `favorite_product_unique` (`user_id`,`product_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `favorite_products`
@@ -43,10 +43,59 @@ CREATE TABLE IF NOT EXISTS `favorite_products` (
 
 INSERT INTO `favorite_products` (`user_id`, `product_id`, `favorite_products_id`) VALUES
 (1, 1, 9),
-(1, 15, 7),
-(50, 1, 10),
-(52, 1, 11),
-(52, 14, 12);
+(1, 15, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `market`
+--
+
+DROP TABLE IF EXISTS `market`;
+CREATE TABLE IF NOT EXISTS `market` (
+  `market_id` int NOT NULL AUTO_INCREMENT,
+  `market_name` varchar(50) NOT NULL,
+  `market_email` varchar(50) NOT NULL,
+  `market_address` varchar(100) NOT NULL,
+  `market_password` varchar(50) NOT NULL,
+  `market_phone` int NOT NULL,
+  `market_photo` varchar(100) NOT NULL,
+  `market_location` varchar(100) NOT NULL,
+  PRIMARY KEY (`market_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `market`
+--
+
+INSERT INTO `market` (`market_id`, `market_name`, `market_email`, `market_address`, `market_password`, `market_phone`, `market_photo`, `market_location`) VALUES
+(3, 'dtwey', 'test2@email', 'xfhb', '123', 416106, '4.png', 'dgcjhdh'),
+(5, 'iyoi', 'sdvfg', 'yoi', '8225', 75272, '4.png', 'uio'),
+(6, 'iyoi', 'sdvfg', 'yoi', '8225', 75272, '4.png', 'uio');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `market_products`
+--
+
+DROP TABLE IF EXISTS `market_products`;
+CREATE TABLE IF NOT EXISTS `market_products` (
+  `market_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `market_products_id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`market_products_id`),
+  UNIQUE KEY `market_id` (`market_id`,`product_id`),
+  UNIQUE KEY `market_id_2` (`market_id`,`product_id`),
+  KEY `product_id` (`product_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `market_products`
+--
+
+INSERT INTO `market_products` (`market_id`, `product_id`, `market_products_id`) VALUES
+(3, 14, 11);
 
 -- --------------------------------------------------------
 
@@ -65,7 +114,7 @@ CREATE TABLE IF NOT EXISTS `product` (
   `product_photo` varchar(200) NOT NULL,
   `product_id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `product`
@@ -74,7 +123,11 @@ CREATE TABLE IF NOT EXISTS `product` (
 INSERT INTO `product` (`product_name`, `product_brand`, `product_price`, `product_brief`, `product_description`, `product_count`, `product_photo`, `product_id`) VALUES
 ('wef', 'wef', 2.2, 'rwegfwr', 'ewfvgwe', 10, '2.png', 1),
 ('gfnjgf', 'dhfmdghm', 535.3, 'sdb', 'sadb', 4, 'Screenshot_20221109_124725.png', 14),
-('gfnjgf', 'dhfmdghm', 535.3, 'sdb', 'sadb', 4, 'Screenshot_20221109_124725.png', 15);
+('gfnjgf', 'dhfmdghm', 535.3, 'sdb', 'sadb', 4, 'Screenshot_20221109_124725.png', 15),
+('dsvewfdas', 'dsvasd', 2.254, 'gkmgewf', 'fhljf', 2, '3.png', 22),
+('dsvewfdas', 'dsvasddwcv', 5.692, 'gkmgewf', 'fhljf', 10, '3.png', 23),
+('dsvewfdas', 'dsvasddwcv', 1000, 'gkmgewf', 'fhljfb,bn', 10, '3.png', 24),
+('pro', 'dsvasddwcv', 2501, 'gkmgewf', 'fhljfb,bn', 15, '4.png', 25);
 
 -- --------------------------------------------------------
 
@@ -93,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_photo` varchar(200) NOT NULL DEFAULT '1000_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg',
   `user_id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `user`
@@ -102,7 +155,9 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`user_email`, `user_password`, `user_name`, `user_address`, `user_location`, `user_phone`, `user_photo`, `user_id`) VALUES
 ('user@email', '12345', 'user1', 'user1address', 'user1location', '021232', 'Screenshot_20221109_124748.png', 1),
 ('user2@email', '98765', 'user2', 'user2Address', 'user2Location', 'user2Phone', '1000_F_339459697_XAFacNQmwnvJRqe1Fe9VOptPWMUxlZP8.jpg', 50),
-('test@email', '123', 'scw', 'dwvwvdhdfbvcx', 'wwcv', '021156', 'Screenshot_20221109_124529.png', 52);
+('test@email', '123', 'scw', 'dwvwvdhdfbvcx', 'wwcv', '021156', 'Screenshot_20221109_124529.png', 52),
+('test2@email', '1234', 'cgnd', 'sdgsdg', 'dfsagf', '2542', '4.png', 53),
+('test2@email', '1234', 'cgnd', 'sdgsdg', 'dfsagf', '2542', '4.png', 54);
 
 --
 -- Constraints for dumped tables
@@ -114,6 +169,13 @@ INSERT INTO `user` (`user_email`, `user_password`, `user_name`, `user_address`, 
 ALTER TABLE `favorite_products`
   ADD CONSTRAINT `favorite_products_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   ADD CONSTRAINT `favorite_products_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `market_products`
+--
+ALTER TABLE `market_products`
+  ADD CONSTRAINT `market_products_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `market_products_ibfk_2` FOREIGN KEY (`market_id`) REFERENCES `market` (`market_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ products_of_market.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if (isset($_SESSION["market_id"])) {
+    $db = mysqli_connect("localhost", "root", "", "ecomm-db");
+    $user_id = $_SESSION["market_id"];
+    echo "<h1>Market Products</h1>";
+    $q = mysqli_query($db, "SELECT p.* FROM market_products fp JOIN product p ON p.product_id = fp.product_id WHERE fp.market_id =$user_id ;");
+    echo "<table border = 2 width = 100% >";
+    echo "<tr><th>product_name</th><th>product_price</th><th>product_brief</th>
+    <th>product_description</th><th>product_photo</th>
+    <th>Navigate to the product</th></tr>";
+    while ($row = mysqli_fetch_array($q)) {
+        $product_id = $row['product_id'];
+        echo "<tr>";
+        echo "<td>" . $row['product_name'];
+        echo "<td>" . $row['product_price'];
+        echo "<td>" . $row['product_brief'];
+        echo "<td>" . $row['product_description'];
+        echo "<td><img src='images/" . $row['product_photo'] . "' width='100' height='100'>";
+        echo "<td><a href='product.php?product_id=$product_id'>View</a>";
+    }
+    echo "</table>";
+    echo '<p><a href="index.php" class="bar-item button">back</a></p>';
+    echo '<p><a href="logout.php" class="bar-item button">log out</a></p>';
+    mysqli_close($db);
+}
+    
+    else {
+        echo "You are not logged in";
+    }
+
+?>
