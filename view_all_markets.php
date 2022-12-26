0ab38593fc183a856b1821813776e98efc1439cf
ibrahimsoltan <70820767+ibrahimsoltan@users.noreply.github.com>
2022-12-27 01:26:47 +0200

    view_all_markets edited

--- view_all_markets.php
+++ view_all_markets.php
@@ -2,8 +2,7 @@
 error_reporting(0);
 session_start();
 
-if (isset($_SESSION["user_id"]))
-{
+
 $msg = "";
 $db = mysqli_connect("localhost", "root", "", "ecomm-db");
 $q = mysqli_query($db, 'SELECT * FROM `market`');
@@ -11,7 +10,7 @@ $q = mysqli_query($db, 'SELECT * FROM `market`');
 echo "<table border = 2 width = 100% >";
 echo "<tr><th>market_name</th><th>market_address</th><th>market_phone</th><th>market_photo</th>
 <th>market_location</th></tr>";
-while ($row=mysqli_fetch_array($q )) {
+while ($row=mysqli_fetch_array($q)) {
     $market_id =$row['market_id'];
     echo "<tr>";
     echo "<td>".$row['market_name'];
@@ -19,15 +18,18 @@ while ($row=mysqli_fetch_array($q )) {
     echo "<td>".$row['market_phone'];
     echo "<td><img src='images/".$row['market_photo']."' width='100' height='100'>";
     echo "<td>".$row['market_location'];
+    if (isset($_SESSION["user_id"]))
+{
     echo "<td><a href='add_to_liked_markets.php?market_id=$market_id' class='bar-item button'>add to liked markets</a></p></td>";
 }
+else{
+    echo "<td><a href='login.html' class='bar-item button'>login to add to liked markets</a></p></td>";
+}
+
+
+}
+mysqli_close($db);
 echo "</table>";
 echo '<p><a href="index.php" class="bar-item button">back</a></p>';
-mysqli_close($db);
-}
-else
-{
-    echo "You are not logged in";
-    echo "<p><a href='login.html'>Login</a></p>";
-}
+
 ?>
